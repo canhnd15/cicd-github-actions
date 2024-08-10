99ddf923345552b922d232ff3e0f88c42ef343f6
canhnd <canhnd15@gmail.com>
2024-08-10 23:31:51 +0700

    [skip ci] - update endpoint

--- src/main/java/com/davidnguyen/cicd/controller/UserController.java
+++ src/main/java/com/davidnguyen/cicd/controller/UserController.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/v1/users")
 public class UserController {
 
     @GetMapping("/")
